a79ba535a91ae704017bd709e949de9282e4998a
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-03-02 09:49:19 -0500

    created a function to write the readme file

--- index.js
+++ index.js
@@ -42,8 +42,12 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if  (err) throw err;
+        console.log("Success!");
+    });
+}
 // TODO: Create a function to initialize app
 function init() {}
 

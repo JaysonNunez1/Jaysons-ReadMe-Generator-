7b64acbfc62c7fb458fc98db1e06604226a904b0
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-03-07 22:29:54 -0500

    changed renderlicensebadge function with an if statement also changed renderlicenselink with an if statement

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,59 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
-  if (!license) {
-    return ''};
+if (license !== "None"){
+  return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
 }
-
-switch (license) {
-  case 'MIT':
-    badge = '[![License:MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    break;
-    case 'Apache 2.0':
-      badge = '[![License:Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-      break;
-      default:
-        badge= '';
-        return badge;
-      }
-const badge = renderLicenseBadge('MIT');
-
+return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!license) {
-    return '';
+  if (license !==  'None') {
+    return `\n* [License](#license)\n`;
   }
+  return " ";
 }
-let link = '';
-switch (license) {
-  case "MIT":
-   link = `https://opensource.org/licenses/MIT`;
-   break;
-   case "Apache 2.0":
-     link = 'https://opensource.org/licenses/Apache-2.0';
-     break;
-     default:
-       link = '';
-       const link = renderLicenseLink("MIT");
-}
-return link;
+
 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license) {
-    return '';
+  if (license !='None') {
+    return `## License 
+    licensed under the ${license} license.`;
   }
-  const licenseLink = renderLicenseLink(license);
-  const licenseBadge = renderLicenseBadge(license);
-  return `## License 
-  This project is licensed under ${license} License. ${licenseBadge} Click [here] (${licenseLink}) to learn more about the license.`;
+  return "";
   }
-  const licenseSection = renderLicenseSection('MIT');
-
+   
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
